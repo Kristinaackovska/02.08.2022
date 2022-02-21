b9b8da28d8da9ccec9677056908850601f9b6ea8
Kristina Ackovska <kristinaackovska88@gmail.com>
2022-02-21 22:44:31 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	boolean flag = true;
+            if(flag == true)
+            if(kristina) {
+                System.out.println("Value of flag is true");
+            } else {
+
+                System.out.println("Value of flag is false");
+
+
+
+            }
+
+
+    }
+
+
+}
+
+
